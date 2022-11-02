f4f2492cf4ec5d71108f3a45dd49d9466a5f235d
Aaron Then <aaronafter@hotmail.com>
2022-11-02 21:40:39 +0800

    adding variable homework

---
+++ solution_variable.py
@@ -0,0 +1,74 @@
+#Decoding R2D2
+#2 beeps, 6 boops (c)
+#0 beeps, 5 boops (c)
+#9 beeps, 3 boops (c)
+#4 beeps, 8 boops (c)
+#10 beeps, 5 boops (c)
+#BOP! (pretty sure this is a space!)(c)
+#11 beeps, 12 boops (c)
+#5 beeps, 5 boops (c)
+#1 beep, 17 boops (c)
+#5 beeps, 7 boops (c)
+#4 beeps, 0 boops
+
+#R2D2 Message is HELLO WJRLD
+
+#H
+beeps = 2
+boops = 6
+total = beeps + boops
+print (total) #prints 8
+
+#E
+beeps = 0
+boops = 5
+total = beeps + boops
+print (total) #prints 5
+
+#L
+beeps = 9
+boops = 3
+total = beeps +boops
+print (total) #prints 12
+
+#L
+beeps = 4
+boops = 8
+total = beeps + boops
+print (total) #prints 12
+
+#O
+beeps = 10
+boops = 5
+total = beeps + boops
+print (total) #prints 15
+
+#W
+beeps = 11
+boops = 12
+total = beeps + boops
+print (total) #prints 23
+
+#J
+beeps = 5
+boops = 5
+total = beeps + boops
+print (total) #prints 10
+
+#R
+beeps = 1
+boops = 17
+total = beeps + boops
+print (total) #print 18
+
+#L
+beeps = 5
+boops = 7
+total = beeps + boops
+print (total) #print 12
+
+#D
+beeps = 4
+boops = 0
+total = beeps + boops
+print (total) #print 4
